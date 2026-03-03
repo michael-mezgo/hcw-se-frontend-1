fb748932fa49068421a23c08e8a7079534bcba83
Michael <58122000+michael-mezgo@users.noreply.github.com>
2026-03-03 20:09:52 +0100

    Add GitHub Action and Test Coverage

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "cypress";
+import codeCoverage from "@cypress/code-coverage/task";
 
 export default defineConfig({
   allowCypressEnv: false,
 
   e2e: {
     baseUrl: "http://localhost:5173",
-    setupNodeEvents() {
-      // implement node event listeners here
+    setupNodeEvents(on, config) {
+      codeCoverage(on, config);
+      return config;
     },
   },
 
@@ -16,5 +18,9 @@ export default defineConfig({
       bundler: "vite",
     },
     specPattern: "cypress/component/**/*.cy.{ts,tsx}",
+    setupNodeEvents(on, config) {
+      codeCoverage(on, config);
+      return config;
+    },
   },
 });

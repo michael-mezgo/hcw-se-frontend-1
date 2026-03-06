ccd15b3e10e3549d9d9dd184e1b25ff5e5015d78
ArAs101 <armin.assadzadeh@stud.hcw.ac.at>
2026-03-06 10:35:38 +0100

    Started implementing a frontend webpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import carPlaceholder from './assets/car_placeholder.png'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    function displayCars(numberOfCars: number) {
+        const cars = [];
+        for (let i = 0; i < numberOfCars; i++) {
+            cars.push(
+                    <div className="car-details">
+                        <div className="car-details-text">
+                            <h3>Car {i + 1}</h3>
+
+                            <p>Brand: {i + 1}</p>
+
+                            <p>Model: {i + 1}</p>
+
+                            <p>Year of manufacture: {i + 1}</p>
+
+                            <p>Price: {i + 1}</p>
+                        </div>
+                        <div className="car-image">
+                            <img src={carPlaceholder} width={300} alt="car_placeholder"/>
+                        </div>
+                    </div>
+            )
+        }
+        return (
+            <div className="car-list">
+                {cars}
+            </div>
+        )
+    }
+
+    return (
+        <>
+            <h2>Browse Cars</h2>
+            <hr/>
+            <br/>
+            {displayCars(10)}
+        </>
+    )
 }
 
 export default App

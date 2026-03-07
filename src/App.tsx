8a8278f91308bccf4ef6e47cb2977c3da38871a7
ArAs101 <armin.assadzadeh@stud.hcw.ac.at>
2026-03-07 14:15:22 +0100

    Created a dummy single-car page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Cars from './pages/Cars'
+import SingleCar from "./pages/SingleCar";
 import NotFound from './pages/NotFound'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cars" element={<Cars />} />
+        <Route path="/cars/:id" element={<SingleCar />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,12 +4,12 @@ function Home() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center p-8">
       <h1 className="text-4xl font-bold text-gray-800 mb-4">Car Rental</h1>
-      <p className="text-gray-600 mb-8">Willkommen bei unserem Autovermietungs-Service.</p>
+      <p className="text-gray-600 mb-8">Welcome to our Car Rental Service.</p>
       <Link
         to="/cars"
         className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition-colors"
       >
-        Alle Fahrzeuge ansehen
+        View all Cars
       </Link>
     </div>
   )

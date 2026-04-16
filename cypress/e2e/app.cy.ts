7601ea0a700f60ad13272d9cee25e4ab1e569f6f
Michael <58122000+michael-mezgo@users.noreply.github.com>
2026-04-16 13:31:29 +0200

    E2E Tests

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,36 +1,50 @@
+const mockCars = Array.from({ length: 10 }, (_, i) => ({
+  id: i + 1,
+  manufacturer: `Brand${i + 1}`,
+  model: `Model${i + 1}`,
+  year: 2020 + i,
+  pricePerDay: 50 + i * 5,
+  description: `Description for car ${i + 1}`,
+  imageUrl: "",
+  transmission: "AUTOMATIC",
+  power: 100 + i * 10,
+  fuelType: "GASOLINE",
+  isAvailable: true,
+  location: { latitude: 48.0, longitude: 16.0 },
+}));
+
 describe("Home page", () => {
   beforeEach(() => {
+    cy.intercept("GET", "/cars", { body: mockCars }).as("getCars");
     cy.visit("/");
   });
 
-  it("displays the Browse Cars heading", () => {
-    cy.get(".browse-cars-header h2").should("contain.text", "Browse Cars");
+  it("displays the Browse Cars link", () => {
+    cy.get("a.browse-cars").should("contain.text", "Browse cars");
   });
 
-  it("displays the car list", () => {
-    cy.get(".car-list").should("exist");
+  it("navigates to the car list on click", () => {
+    cy.get("a.browse-cars").click();
+    cy.wait("@getCars");
+    cy.get("ul").should("exist");
   });
 
   it("displays 10 car entries", () => {
-    cy.get(".car-details").should("have.length", 10);
+    cy.get("a.browse-cars").click();
+    cy.wait("@getCars");
+    cy.get("ul li").should("have.length", 10);
   });
 
-  it("displays car titles with correct numbering", () => {
-    cy.get(".car-details").first().find("h3").should("contain.text", "Car 1");
-    cy.get(".car-details").last().find("h3").should("contain.text", "Car 10");
+  it("displays manufacturer and model for each car", () => {
+    cy.get("a.browse-cars").click();
+    cy.wait("@getCars");
+    cy.get("ul li").first().should("contain.text", "Brand1").and("contain.text", "Model1");
+    cy.get("ul li").last().should("contain.text", "Brand10").and("contain.text", "Model10");
   });
 
-  it("displays car details text for each car", () => {
-    cy.get(".car-details").first().within(() => {
-      cy.get(".car-details-text").should("exist");
-      cy.get("p").should("have.length.gte", 4);
-    });
-  });
-
-  it("displays a placeholder image for each car", () => {
-    cy.get(".car-details img[alt='Placeholder car image']").should(
-      "have.length",
-      10
-    );
+  it("displays price per day for each car", () => {
+    cy.get("a.browse-cars").click();
+    cy.wait("@getCars");
+    cy.get("ul li").first().should("contain.text", "50€ / Day");
   });
 });
